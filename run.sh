f842cd694f58b61ed923b4265371e092d64c92c2
PangBo <51732678+pangbo13@users.noreply.github.com>
2023-06-08 21:36:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ read endtime
 echo -n "联系方式："
 read contact
 
-sed -e "s/{time}/$endtime/g" ./errorpages/index.template.html | sed -e "s/{contact}/$contact/g" >> ./errorpages/index.html
+sed -e "s/{time}/$endtime/g" ./errorpages/index.template.html | sed -e "s/{contact}/$contact/g" > ./errorpages/index.html
 echo "已输出 index.html 文件"
